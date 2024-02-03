0430720ce64ff00117f5378d1e0d1f6b2dd5d699
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2024-02-03 09:57:07 -0600

    Initial code that gets assemblies that match a pattern

--- ArchiTest/Verify.cs
+++ ArchiTest/Verify.cs
@@ -6,70 +6,58 @@ using System.Text.RegularExpressions;
 
 namespace SampleProject
 {
-    public class Verify
+    public static class Verify
     {
-        private List<Assembly> _assemblies = new List<Assembly>();
-
-        #region Constructors
-
-        private Verify()
+        public static IEnumerable<Assembly> InAssemblies(params string[] assemblyNamePatterns)
         {
+            var allAssemblies =
+                AppDomain.CurrentDomain.GetAssemblies().ToList();
+
+            if (assemblyNamePatterns == null)
+            {
+                return allAssemblies;
+            }
+
+            List<Regex> regexPatterns = CreateRegexPatterns(assemblyNamePatterns);
+
+            var matchingAssemblies = new List<Assembly>();
+
+            // Check each assembly against the patterns
+            foreach (var assembly in allAssemblies)
+            {
+                string assemblyName = assembly.GetName().Name;
+
+                foreach (var regex in regexPatterns)
+                {
+                    if (regex.IsMatch(assemblyName))
+                    {
+                        matchingAssemblies.Add(assembly);
+                        break; // Break to avoid adding the same assembly multiple times
+                    }
+                }
+            }
+
+            return matchingAssemblies;
         }
 
-        public static Verify That()
+        #region Private Methods
+
+        private static List<Regex> CreateRegexPatterns(string[] namePatterns)
         {
-            return new Verify();
+            var regexPatterns = new List<Regex>();
+
+            foreach (var pattern in namePatterns)
+            {
+                var regexPattern =
+                    new Regex("^" + Regex.Escape(pattern).Replace("\\*", ".*").Replace("\\?", ".") + "$",
+                    RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+                regexPatterns.Add(regexPattern);
+            }
+
+            return regexPatterns;
         }
 
         #endregion
-
-        public IEnumerable<Assembly> Assemblies(params string[] namePatterns)
-        {
-            var assemblies = AppDomain.CurrentDomain.GetAssemblies().ToList();
-
-            if (namePatterns == null)
-            {
-                _assemblies = assemblies;
-            }
-            else
-            {
-                // Convert wildcard patterns to regular expressions
-                var regexPatterns = new List<Regex>();
-
-                foreach (var pattern in namePatterns)
-                {
-                    regexPatterns.Add(ConvertWildcardToRegex(pattern));
-                }
-
-                // Check each assembly against the patterns
-                foreach (var assembly in assemblies)
-                {
-                    if (_assemblies.Contains(assembly))
-                    {
-                        continue; // Skip already added assemblies   
-                    }
-
-                    foreach (var regex in regexPatterns)
-                    {
-                        if (regex.IsMatch(assembly.GetName().Name))
-                        {
-                            _assemblies.Add(assembly);
-                            break; // Break to avoid adding the same assembly multiple times
-                        }
-                    }
-                }
-
-            }
-
-            return _assemblies;
-        }
-
-        public static Regex ConvertWildcardToRegex(string pattern)
-        {
-            string regexPattern = 
-                "^" + Regex.Escape(pattern).Replace("\\*", ".*").Replace("\\?", ".") + "$";
-
-            return new Regex(regexPattern, RegexOptions.IgnoreCase);
-        }
     }
 }
--- Test.ArchiTest/TestArchiTest.cs
+++ Test.ArchiTest/TestArchiTest.cs
@@ -5,23 +5,44 @@ namespace Test.ArchiTest;
 public class TestArchiTest
 {
     [Fact]
-    public void Test_Assemblies_SpecificName()
+    public void Test_Assemblies_SpecificName1()
     {
-        var assemblies = Verify.That().Assemblies("ArchiTest.SampleProject");
+        var assemblies = Verify.InAssemblies("ArchiTest");
         Assert.Single(assemblies);
+    }
+
+    [Fact]
+    public void Test_Assemblies_SpecificName2()
+    {
+        var assemblies = Verify.InAssemblies("ArchiTest.SampleProject");
+        Assert.Single(assemblies);
+    }
+
+    [Fact]
+    public void Test_Assemblies_SpecificNames()
+    {
+        var assemblies = Verify.InAssemblies("ArchiTest", "ArchiTest.SampleProject");
+        Assert.Equal(2, assemblies.Count());
     }
 
     [Fact]
     public void Test_Assemblies_Wildcard_Single()
     {
-        var assemblies = Verify.That().Assemblies("ArchiTest.*");
+        var assemblies = Verify.InAssemblies("ArchiTest.*");
         Assert.Single(assemblies);
     }
 
     [Fact]
     public void Test_Assemblies_Wildcard_Double()
     {
-        var assemblies = Verify.That().Assemblies("ArchiTest*");
+        var assemblies = Verify.InAssemblies("ArchiTest*");
         Assert.Equal(2, assemblies.Count());
+    }
+
+    [Fact]
+    public void Test_Assemblies_NoMatch()
+    {
+        var assemblies = Verify.InAssemblies("iojwfoiej390248230948");
+        Assert.Empty(assemblies);
     }
 }
